644fd5ac8f1dc1ccf49b7c0dfcd8461bac5f35ba
Chris Dickinson <christopher.s.dickinson@gmail.com>
2009-12-01 18:08:36 -0600

    links on front page work now

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from radio_station.models import Spot
+from radio_station.models import Spot, Schedule
 from radio_events.models import Event
 from django.template import loader, RequestContext
 from django import http
@@ -13,6 +13,7 @@ def home(request):
     import datetime
     now = datetime.datetime.now()
 
+    schedule = Schedule.objects.get_current_schedule()
     current_spot = Spot.objects.get_current_spot(now)
     next_spots = Spot.objects.filter_next_spots(now)[:6]
     weekday_str = 'MTWRFSU'[current_spot.to_datetime().weekday()]
@@ -28,6 +29,8 @@ def home(request):
         'day_after':Event.objects.filter(date=day_after).order_by('-weight')[:3],
     }
 
+    start_of_week = now - datetime.timedelta(days=now.weekday()) 
+
     ctxt = {
         'today':now,
         'tomorrow':now+datetime.timedelta(days=1),
@@ -36,5 +39,8 @@ def home(request):
         'next_spots':next_spots,
         'weekday_str':weekday_str,
         'events':events,
+        'schedule':schedule,
+        'week':[(start_of_week + datetime.timedelta(days=i)).date() for i in range(0, 7)],
+        'now':now.date()
     }
     return render_to_response('home.html', ctxt, context_instance=RequestContext(request)) 
