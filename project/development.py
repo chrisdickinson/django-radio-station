2179e5f45b7067819dfd380c338034fb7d2a88b5
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-02-19 15:40:58 -0600

    add in support for local_development.py

--- project/development.py
+++ project/development.py
@@ -1,4 +1,7 @@
-
 from project.settings import *
-DEBUG=True
+
+DEBUG=False
 TEMPLATE_DEBUG=DEBUG
+
+if os.path.exists(os.path.join(os.path.dirname(__file__), "local_development.py")):
+    from local_development import *
