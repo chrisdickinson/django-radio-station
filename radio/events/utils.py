d955d2bbcf40abdb3aa408abf9e18ff97c997b4d
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-02-19 11:54:39 -0600

    moved get_when_or_now from radio.events.utils to radio.datetime

--- radio/events/utils.py
+++ radio/events/utils.py
@@ -1,7 +1,1 @@
-import datetime
-
-def get_when_or_now(*args):
-    if None in args:
-        return datetime.datetime.now()
-    return datetime.datetime(*[int(arg) for arg in args])
-
+from radio.datetime import get_when_or_now
