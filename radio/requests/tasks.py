113b8180beca4836a211f4e3be2f63f857a86680
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-30 14:04:07 -0500

    get rid of extra radio subdirectory

--- radio/requests/tasks.py
+++
@@ -1,7 +0,0 @@
-def post_responses(requests):
-    for request in requests:
-        if request.dm_on_fulfilled:
-            waiter = waiter.Waiter(menu_class=TwitterMenu, consumer=TwitterConsumer())
-            waiter/"messages/create.json"/{
-                'to_user':user.twitter.info.data.screen_name,
-            }
